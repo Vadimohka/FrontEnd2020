ef8ef3910c0b28b8d6a5b2d263a5d946d4e3dcad
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-09-15 22:35:38 +0300

    add: swiperjs fully working with custom rendered pagination

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
+var menu = ['Брест', 'Витебск', 'Могилев', 'Минск', 'Гродно', 'Гомель', 'Минская Обл']
 var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    
-    autoplay: {
-        delay: 2000,
+  // Optional parameters
+  speed: 1500,
+  direction: 'horizontal',
+  loop: true,
+
+  //autoplay: {
+//    delay: 2000,
+//  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (menu[index]) + '</span>';
       },
-    
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
