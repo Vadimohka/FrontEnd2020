18fd72134ba6605d4013bbc4677065149e2d5866
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-11-19 14:53:11 +0300

    upd: adaptive info-paragraph for width less then 660px

--- js/showinfo.js
+++ js/showinfo.js
@@ -1,10 +1,12 @@
 function showinfo(city){
     console.log(city);
     let cities = ['BrestInfo', 'VitebskInfo', 'MogilevInfo', 'MinskInfo', 'GrodnoInfo', 'GomelInfo', 'MinskayaInfo'];
+    let infos = ['BrestInformation','VitebskInformation','MogilevInformation','MinskInformation','GrodnoInformation','GomelInformation','MinskayaInformation'];
     let logos = ['BrestLogo', 'VitebskLogo', 'MogilevLogo', 'MinskLogo', 'GrodnoLogo', 'GomelLogo', 'MinskayaLogo'];
-    for (let i = 0; i < cities.length, i++;){
+    for (let i = 0; i < cities.length; i++){
         if (city===cities[i]){
-            document.getElementById(logos[i]).style.display = 'block';
+            document.getElementById(logos[i]).style.display = 'none';
+            document.getElementById(infos[i]).style.display = 'block';
         }
     }
 }
