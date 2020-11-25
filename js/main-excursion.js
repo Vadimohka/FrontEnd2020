641bafb42c29c3a505b5365fd318492d801e5a72
SQflex <32853595+Sqflex@users.noreply.github.com>
2020-11-25 15:22:32 +0300

    add: basic animation for info and logo

--- js/main-excursion.js
+++ js/main-excursion.js
@@ -6,7 +6,7 @@ var mySwiper = new Swiper('.swiper-container', {
   loop: true,
 
   autoplay: {
-    delay: 2000,
+    delay: 10000,
    },
 
   // If we need pagination
--- js/showinfo.js
+++ js/showinfo.js
@@ -4,8 +4,10 @@ function showinfo(city){
     let logos = ['BrestLogo', 'VitebskLogo', 'MogilevLogo', 'MinskLogo', 'GrodnoLogo', 'GomelLogo', 'MinskayaLogo'];
     for (let i = 0; i < cities.length; i++){
         if (city===cities[i]){
-            document.getElementById(logos[i]).style.display = 'none';
-            document.getElementById(infos[i]).style.display = 'block';
+            document.getElementById(logos[i]).style.opacity = '0';
+            setTimeout( function(){document.getElementById(logos[i]).style.display = 'none';
+            document.getElementById(infos[i]).style.display = 'block';}, 400);
+            document.getElementById(infos[i]).style.opacity = '1';
         }
     }
 }
@@ -16,8 +18,10 @@ function closeinfo(logo_id){
     let buttons = ['Brest-close-button', 'Vitebsk-close-button', 'Mogilev-close-button','Minsk-close-button', 'Grodno-close-button', 'Gomel-close-button','Minskaya-close-button'];
     for(let i = 0; i < buttons.length; i++){
         if(logo_id===buttons[i]){
-            document.getElementById(logos[i]).style.display = 'block';
-            document.getElementById(infos[i]).style.display = 'none';
+            document.getElementById(infos[i]).style.opacity = '0';            
+            setTimeout(function() {document.getElementById(logos[i]).style.display = 'block';
+            document.getElementById(infos[i]).style.display = 'none';}, 400);
+            document.getElementById(logos[i]).style.opacity = '1';
         }
     }
 }
